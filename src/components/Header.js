ee2788636aa71944a7a5cc14143aa480d3f59234
ashutosh jaiswal <jaiswalashutosh08@gmail.com>
2022-07-18 01:20:45 +0530

    header-complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,17 @@ export default function Header() {
     <>
     <div className = 'header-container'>
         <div className="image-logo">
-            <img src = '../../images/header/pepc_logo.png'/>
+            <img src = '../../images/header/pepc_logo.png' alt= "pepcode logo"/>
         </div>
         <div className="l">Trainings</div>
-        <div className="l">Garanteed Job Program</div>
+        <div className="l header-gjp">Garanteed Job Program</div>
         <div className="l">NADOS</div>
         <div className="l">Resousces</div>
         <div className="l header-login">Login</div>
+    </div>
+    <div className="form-section">
+        <div className='about'></div>
+        <div className=''></div>
     </div>
     </>
   )
